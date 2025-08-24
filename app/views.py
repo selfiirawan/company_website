f1b3aeecce001e3a2e0b5b8b3ee43fe4bdd7da10
Selfi <celpi.irawan@gmail.com>
2025-08-24 16:03:51 +0800

    html template inheritance

--- app/views.py
+++ app/views.py
@@ -3,14 +3,9 @@ from datetime import datetime
 
 # Create your views here.
 def index(request):
-    context = {
-        "course_title": "Django Course",
-        "current_date": datetime.now(),
-        "user": {
-            "name": "John Doe",
-            "email": "john.doe@example.com",
-        },
-        "price": 199.9999,
-        "text": "Welcome to the Django Course!"
-    }
+    context = {}
     return render(request, "index.html", context)
+
+def aboutus(request):
+    context = {}
+    return render(request, 'aboutus.html', context)
--- company/urls.py
+++ company/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from app.views import index
+from app.views import index, aboutus
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", index)
+    path("", index, name="home"),
+    path("aboutus/", aboutus, name="aboutus")
 ]
